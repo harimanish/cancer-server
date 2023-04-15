92e6d7329b5d1c6bbe86395ecf0248f78319e898
Manish <harimanishhmp@gmail.com>
2023-04-15 23:09:16 +0530

    target classes

--- functions/index.js
+++ functions/index.js
@@ -18,13 +18,13 @@ app.use((req, res, next) => {
 });
 
 const TARGET_CLASSES = {
-  0: 'akiec, Actinic Keratoses (Solar Keratoses) or intraepithelial Carcinoma (Bowen’s disease)',
-  1: 'bcc, Basal Cell Carcinoma',
-  2: 'bkl, Benign Keratosis',
-  3: 'df, Dermatofibroma',
-  4: 'mel, Melanoma',
-  5: 'nv, Melanocytic Nevi',
-  6: 'vasc, Vascular skin lesion'
+  0: 'Actinic Keratoses (Solar Keratoses) or intraepithelial Carcinoma (Bowen’s disease)',
+  1: 'Basal Cell Carcinoma',
+  2: 'Benign Keratosis',
+  3: 'Dermatofibroma',
+  4: 'Melanoma',
+  5: 'Melanocytic Nevi',
+  6: 'Vascular skin lesion'
 };
 
 const loadModel = async () => {
